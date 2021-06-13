c0034fc02e8c07ac160be26ada7c735580727635
tunishq <74109308+tunishq@users.noreply.github.com>
2021-06-13 14:44:32 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,13 +7,36 @@ function setup()
 {
  canvas = createCanvas(300, 300);
  canvas.center();
+ video = createCapture(VIDEO);
+ video.size(300, 300);
+ video.hide();
+ 
+poseNet = ml5.poseNet(video, modelLoaded); 
+poseNet.on('pose', gotPoses);
 }
+
+function modelLoaded()
+{
+    console.log('Posenet is initialised');
+}
+
 
 function draw()
 {
-
+image(video, 0, 0, 300, 300);
 }
+
 function take_snapshot()
 {
     save('filteredImage.png');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+    }
 }
